c937b5beeb27474cc1b0979bb67c36c7c83f7da2
KinlyDog <kinlydog@gmail.com>
2023-05-03 20:15:49 +1000

    Update project

--- src/BalancedBST.java
+++ src/BalancedBST.java
@@ -1,22 +1,23 @@
 import java.util.*;
 
 class BSTNode {
-    public int NodeKey;
-    public BSTNode Parent;
-    public BSTNode LeftChild;
-    public BSTNode RightChild;
-    public int Level;
+    public int NodeKey; // ключ узла
+    public BSTNode Parent; // родитель или null для корня
+    public BSTNode LeftChild; // левый потомок
+    public BSTNode RightChild; // правый потомок
+    public int Level; // глубина узла
 
     public BSTNode(int key, BSTNode parent) {
         NodeKey = key;
         Parent = parent;
         LeftChild = null;
         RightChild = null;
+        Level = 0;
     }
 }
 
 class BalancedBST {
-    public BSTNode Root;
+    public BSTNode Root; // корень дерева
 
     public BalancedBST() {
         Root = null;
@@ -24,21 +25,20 @@ class BalancedBST {
 
     public void GenerateTree(int[] a) {
         Arrays.sort(a);
-
         Root = GenerateTreeRec(a, 0, a.length - 1, null, 0);
     }
 
-    private BSTNode GenerateTreeRec(int[] a, int start, int end, BSTNode parent, int level) {
-        if (start > end) {
+    private BSTNode GenerateTreeRec(int[] a, int startIndex, int endIndex, BSTNode parent, int level) {
+        if (startIndex > endIndex) {
             return null;
         }
 
-        int mid = (start + end) / 2;
+        int mid = startIndex + (endIndex - startIndex) / 2;
         BSTNode node = new BSTNode(a[mid], parent);
         node.Level = level;
 
-        node.LeftChild = GenerateTreeRec(a, start, mid - 1, node, level + 1);
-        node.RightChild = GenerateTreeRec(a, mid + 1, end, node, level + 1);
+        node.LeftChild = GenerateTreeRec(a, startIndex, mid - 1, node, level + 1);
+        node.RightChild = GenerateTreeRec(a, mid + 1, endIndex, node, level + 1);
 
         return node;
     }
@@ -48,8 +48,8 @@ class BalancedBST {
             return true;
         }
 
-        int leftDepth = GetDepth(node.LeftChild);
-        int rightDepth = GetDepth(node.RightChild);
+        int leftDepth = getDepth(node.LeftChild);
+        int rightDepth = getDepth(node.RightChild);
 
         if (Math.abs(leftDepth - rightDepth) > 1) {
             return false;
@@ -58,11 +58,14 @@ class BalancedBST {
         return IsBalanced(node.LeftChild) && IsBalanced(node.RightChild);
     }
 
-    private int GetDepth(BSTNode node) {
+    private int getDepth(BSTNode node) {
         if (node == null) {
             return 0;
         }
 
-        return node.Level;
+        int leftDepth = getDepth(node.LeftChild);
+        int rightDepth = getDepth(node.RightChild);
+
+        return Math.max(leftDepth, rightDepth) + 1;
     }
 }
